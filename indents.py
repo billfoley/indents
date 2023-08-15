05181664b76e873ea9f5da362c868d96cc901a54
billfoley <wvfoley@gmail.com>
2023-08-15 16:31:40 -0400

    Tests passed

--- indents.py
+++ indents.py
@@ -2,4 +2,12 @@ def count_indents(text: str) -> int:
     """
     Count and return the number of leading white space characters (' ').
     """
-    pass
+    count = 0
+
+    for i in text:
+        if i != " ":
+            break
+        else:
+            count += 1
+
+    return count
